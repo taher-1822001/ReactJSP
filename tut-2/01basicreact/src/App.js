39b27ee4691d71b7d87e0c06684a4876a10a962e
Taher Ahmed <taher1822001@gmail.com>
2023-10-22 12:20:31 +0000

    t-2

---
+++ tut-2/01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>Taher Ahmed | </h1>
+  );
+}
+
+export default App;
---
+++ tut-2/01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+ 
+  return (
+
+      <h1>Vite project | Taher Ahmed</h1>
+
+  )
+}
+
+export default App
